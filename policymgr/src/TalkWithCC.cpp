17f133a972bae2c70f57a82961dd3c04099bfb59
jeff <Jeff.Zhu@nextlabs.com>
2019-10-25 18:38:15 +0800

    add search policy api

--- policymgr/src/TalkWithCC.cpp
+++ policymgr/src/TalkWithCC.cpp
@@ -272,6 +272,19 @@ bool TalkWithCCLower::SearchComponentByID(const std::string& id, std::string& ou
     return false;
 }
 
+ bool TalkWithCCLower::SearchPolicyIDsByTag(const std::string& tag, std::vector<std::string>& out) {
+    return true;/*todo*/
+}
+ bool TalkWithCCLower::SearchPolicyByID(const std::string& id, std::string& out) {
+    return true;/*todo*/
+}
+ bool TalkWithCCLower::SearchPolicyModelByID(const std::string& id, std::string& out) {
+    return true;/*todo*/
+}
+ bool TalkWithCCLower::SearchPolicyModelByName(const std::string& name, std::string& out) {
+    return true;/*todo*/
+}
+
 std::string TalkWithCCLower::CtorLoginBody() {
     std::string body;
     body += "username=" + _user; body += "&password=" + _pwd;
